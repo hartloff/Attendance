10966012ecbf99f7c1bacd3fe66d5053a1d33159
jessehartloff <hartloff@buffalo.edu>
2016-11-05 21:09:11 -0400

    should be working (with test files)

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/test');
+//var mongo = require('mongodb');
+//var monk = require('monk');
+//var db = monk('localhost:27017/test');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,10 +28,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(function(req,res,next){
-  req.db = db;
-  next();
-});
+//app.use(function(req,res,next){
+//  req.db = db;
+//  next();
+//});
 
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,48 @@ var router = express.Router();
 var app = express();
 
 function scan_to_ubit(scan) {
-    var person_number = 2;
+    var splits = scan.split('^');
+    if (splits.length < 3) {
+        return [scan, scan, scan];
+    }
+    var person_number = splits[2];
+    if (person_number.length < 23) {
+        return [scan, scan, scan];
+    }
+    person_number = person_number.substring(14, 22);
+    var name = person_number;
+    var ubit = person_number;
 
-    return 'error';
+    var fileContent = fs.readFileSync(__dirname + '/roster').toString()
+    console.log(fileContent);
+    var lines = fileContent.split('\n');
+    for (var i in lines) {
+        var line = lines[i];
+        var values = line.split('\t');
+        console.log(values);
+        console.log(person_number);
+        if(values[0] === person_number) {
+            ubit = values[2];
+            name = values[1];
+        }
+    }
+
+    return [ubit, name, person_number];
 }
 
 /* GET home page. */
 router.get('/', function (req, res) {
 
-    var ubit = "nothing";
-    var db = req.db;
-    var collection = db.get('stuff');
-    collection.find({}, {}, function (e, docs) {
-        ubit = docs.toJSON();
-    });
+    //var ubit = "nothing";
+    //var db = req.db;
+    //var collection = db.get('stuff');
+    //collection.find({}, {}, function (e, docs) {
+    //    ubit = docs.toJSON();
+    //});
 
     res.render('index', {
         title: 'Scan For Credit',
-        last_user: collection.toString(),
-        image_url: 'https://upload.wikimedia.org/wikipedia/commons/b/b5/Xkcd_philosophy.png'
+        last_user: ''
     });
 });
 
@@ -33,20 +56,20 @@ router.post('/', function (req, res) {
 
     var dir = __dirname;
     var filename = "/hackathon";
-    var the_response = 'error';
 
     var ubit = scan_to_ubit(req.body.scan);
-    if (ubit == "error") {
-        ubit = req.body.scan;
+    console.log(ubit);
+    var name = ubit;
+    if (ubit[0].length > 1) {
+        name = ubit[1];
+        ubit = ubit[0];
     }
-
-
-    fs.appendFile(dir + filename, '\n' + ubit);
-
+    if (ubit != '') {
+        fs.appendFile(dir + filename, '\n' + ubit);
+    }
     res.render('index', {
         title: 'Scan For Credit',
-        last_user: ubit,
-        image_url: 'http://imgs.xkcd.com/comics/old_days_2x.png'
+        last_user: name
     });
 });
 
